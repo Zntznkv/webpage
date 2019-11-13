e32b1fa955799b818ed2669b2517eb0a03cd0025
Zaneta Zeleznikova <zzaneta.zeleznikova@gmail.com>
2019-11-13 18:18:48 +0100

    nova hodina array reduce

--- src/p1.js
+++ src/p1.js
@@ -17,4 +17,15 @@ var person = {
   };
   console.log(person.firstName);
   console.log(person.fullName());
-  
+  const p = ['ja', 'ty', 'ono'];
+  console.log(p[1]);
+  console.log(person["firstName"]);
+  console.log(person.firstName);
+console.log(person.lastName.length);
+p.forEach((value, index ) => console.log(value, index))
+const p1 = p.map(value => value.length);
+console.log(p1);
+const p2 = p.filter((value) => value.length < 3);
+console.log(p2);
+const p3 = p.reduce((total, value ) => total + value.length, 0);
+console.log(p3);
